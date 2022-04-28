bbe450ad80682114c2728d6e4e500d0c17446000
rio.watanabe <rio.watanabe@mixi.co.jp>
2022-04-28 06:38:58 +0000

    update solution 2022 and change ai platform to vertex ai

--- solutions/03_parameter_tuning/mfashion_keras/callback.py
+++ solutions/03_parameter_tuning/mfashion_keras/callback.py
@@ -1,5 +1,13 @@
 import tensorflow as tf
+import hypertune
+
+hpt = hypertune.HyperTune()
+
 
 class MyMetricCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
-         tf.summary.scalar('ccentropy', logs['categorical_crossentropy'], epoch)
+        hpt.report_hyperparameter_tuning_metric(
+            hyperparameter_metric_tag='ccentropy', 
+            metric_value=logs['categorical_crossentropy'], 
+            global_step=epoch
+        )
